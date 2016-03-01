6e3a92b1b853cc9c7afde6f4b85134b91621f440
MIGUEL <miguel@miguels-mac.local>
2016-02-29 19:25:18 -0500

    Initial Commit

---
+++ Juegodememoria/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Juegodememoria
+//
+//  Created by MIGUEL on 29/02/16.
+//  Copyright © 2016 MIGUEL. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
